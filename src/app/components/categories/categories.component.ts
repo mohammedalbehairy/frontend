96b6f7655344b4fd76de28252b111717f8b2aa41
mohammedalbehairy <mohammedalbehairy90@gmail.com>
2021-07-10 12:25:54 +0200

    Add categories slider

--- src/app/components/categories/categories.component.ts
+++ src/app/components/categories/categories.component.ts
@@ -7,35 +7,233 @@ import { SwiperOptions } from 'swiper';
   styleUrls: ['./categories.component.scss'],
 })
 export class CategoriesComponent implements OnInit {
-  Images = [
-    {
-      src: 'https://loremflickr.com/g/600/400/paris',
-      alt: 'Image 1',
-    },
-    {
-      src: 'https://loremflickr.com/600/400/brazil,rio',
-      alt: 'Image 2',
-    },
-    {
-      src: 'https://loremflickr.com/600/400/paris,girl/all',
-      alt: 'Image 3',
-    },
-    {
-      src: 'https://loremflickr.com/600/400/brazil,rio',
-      alt: 'Image 4',
-    },
-    {
-      src: 'https://loremflickr.com/600/400/paris,girl/all',
-      alt: 'Image 5',
-    },
-    {
-      src: 'https://loremflickr.com/600/400/brazil,rio',
-      alt: 'Image 6',
-    },
+  arrFarms = [
+    'Vegetables',
+    'Fresh Box',
+    'Fruits',
+    'Herbs',
+    'Hydroponic Farming',
   ];
+  arrFarmsPrev = 0;
+  arrFarmsActive = 1;
+  arrFarmsNext = 2;
+
+  arrDelights = [
+    'Bread and Rolls',
+    'Biscuits',
+    'Cakes, Cupcake and Muffins',
+    'Croissants and Pastries',
+    'Crackers, Bread Sticks and Crispbread',
+    'Donuts, Cookies and Muffins',
+    'Food To Go',
+  ];
+  arrDelightsPrev = 0;
+  arrDelightsActive = 1;
+  arrDelightsNext = 2;
+
+  arrButchers = [
+    'Beef',
+    'Chicken',
+    'Lamb',
+    'Offal',
+    'Turkey',
+    'Variety Meat',
+    'Veal',
+  ];
+  arrButchersPrev = 0;
+  arrButchersActive = 1;
+  arrButchersNext = 2;
+
+  arrSeaFood = ['Fish', 'Food To Go', 'Seafood'];
+  arrSeaFoodPrev = 0;
+  arrSeaFoodActive = 1;
+  arrSeaFoodNext = 2;
+
+  arrGrater = [
+    'Cheese',
+    'Chilled Desserts',
+    'Cold Cuts & Meat Snacks',
+    'Cream & Labneh',
+    'Dips, Spreads & Pate',
+    'Kimchi',
+    'Olives & Antipasti',
+    'Tofu',
+  ];
+  arrGraterPrev = 0;
+  arrGraterActive = 1;
+  arrGraterNext = 2;
 
   constructor() {}
 
   ngOnInit(): void {}
 
+  //#region farms
+  incrementFarms() {
+    if (this.arrFarmsActive == this.arrFarms.length - 1) {
+      this.arrFarmsActive = 0;
+    } else {
+      this.arrFarmsActive++;
+    }
+  }
+
+  decrementFarms() {
+    if (this.arrFarmsActive == 0) {
+      this.arrFarmsActive = this.arrFarms.length - 1;
+    } else {
+      this.arrFarmsActive--;
+    }
+  }
+
+  get prevIndexFarms() {
+    if (this.arrFarmsActive == 0) {
+      return this.arrFarms.length - 1;
+    } else {
+      return this.arrFarmsActive - 1;
+    }
+  }
+
+  get nextIndexFarms() {
+    if (this.arrFarmsActive == this.arrFarms.length - 1) {
+      return 0;
+    } else {
+      return this.arrFarmsActive + 1;
+    }
+  }
+  //#endregion
+
+  //#region Delights
+  incrementDelights() {
+    if (this.arrDelightsActive == this.arrButchers.length - 1) {
+      this.arrDelightsActive = 0;
+    } else {
+      this.arrDelightsActive++;
+    }
+  }
+
+  decrementDelights() {
+    if (this.arrDelightsActive == 0) {
+      this.arrDelightsActive = this.arrButchers.length - 1;
+    } else {
+      this.arrDelightsActive--;
+    }
+  }
+
+  get prevIndexDelights() {
+    if (this.arrDelightsActive == 0) {
+      return this.arrButchers.length - 1;
+    } else {
+      return this.arrDelightsActive - 1;
+    }
+  }
+
+  get nextIndexDelights() {
+    if (this.arrDelightsActive == this.arrButchers.length - 1) {
+      return 0;
+    } else {
+      return this.arrDelightsActive + 1;
+    }
+  }
+  //#endregion
+
+  //#region Butchers
+  incrementButchers() {
+    if (this.arrButchersActive == this.arrButchers.length - 1) {
+      this.arrButchersActive = 0;
+    } else {
+      this.arrButchersActive++;
+    }
+  }
+
+  decrementButchers() {
+    if (this.arrButchersActive == 0) {
+      this.arrButchersActive = this.arrButchers.length - 1;
+    } else {
+      this.arrButchersActive--;
+    }
+  }
+
+  get prevIndexButchers() {
+    if (this.arrButchersActive == 0) {
+      return this.arrButchers.length - 1;
+    } else {
+      return this.arrButchersActive - 1;
+    }
+  }
+
+  get nextIndexButchers() {
+    if (this.arrButchersActive == this.arrButchers.length - 1) {
+      return 0;
+    } else {
+      return this.arrButchersActive + 1;
+    }
+  }
+  //#endregion
+
+  //#region SeaFood
+  incrementSeaFood() {
+    if (this.arrSeaFoodActive == this.arrSeaFood.length - 1) {
+      this.arrSeaFoodActive = 0;
+    } else {
+      this.arrSeaFoodActive++;
+    }
+  }
+
+  decrementSeaFood() {
+    if (this.arrSeaFoodActive == 0) {
+      this.arrSeaFoodActive = this.arrSeaFood.length - 1;
+    } else {
+      this.arrSeaFoodActive--;
+    }
+  }
+
+  get prevIndexSeaFood() {
+    if (this.arrSeaFoodActive == 0) {
+      return this.arrSeaFood.length - 1;
+    } else {
+      return this.arrSeaFoodActive - 1;
+    }
+  }
+
+  get nextIndexSeaFood() {
+    if (this.arrSeaFoodActive == this.arrSeaFood.length - 1) {
+      return 0;
+    } else {
+      return this.arrSeaFoodActive + 1;
+    }
+  }
+  //#endregion
+
+  //#region Grater
+  incrementGrater() {
+    if (this.arrGraterActive == this.arrGrater.length - 1) {
+      this.arrGraterActive = 0;
+    } else {
+      this.arrGraterActive++;
+    }
+  }
+
+  decrementGrater() {
+    if (this.arrGraterActive == 0) {
+      this.arrGraterActive = this.arrGrater.length - 1;
+    } else {
+      this.arrGraterActive--;
+    }
+  }
+
+  get prevIndexGrater() {
+    if (this.arrGraterActive == 0) {
+      return this.arrGrater.length - 1;
+    } else {
+      return this.arrGraterActive - 1;
+    }
+  }
+
+  get nextIndexGrater() {
+    if (this.arrGraterActive == this.arrGrater.length - 1) {
+      return 0;
+    } else {
+      return this.arrGraterActive + 1;
+    }
+  }
+  //#endregion
 }
